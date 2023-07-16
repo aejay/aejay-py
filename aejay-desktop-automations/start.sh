3c14a911d59ff436923e721c476810a2bf4183da
Aejay Goehring <aejay.goehring@gmail.com>
2023-07-16 10:52:05 -0500

    Tweak start script.

--- aejay-desktop-automations/start.sh
+++ aejay-desktop-automations/start.sh
@@ -1,13 +1,9 @@
-#!/bin/sh
-# Source the appropriate profile file
-if [ -n "$ZSH_VERSION" ]; then
-    source ~/.zshrc
-elif [ -n "$BASH_VERSION" ]; then
-    source ~/.bashrc
-else
-    echo "Script only supports bash and zsh"
-    exit 1
+#!/bin/zsh
+
+if [[ -f ~/.zshrc ]]; then
+  source ~/.zshrc
 fi
 
-# Now we can use the asdf shims
-python /Users/aejay/Repos/aejay-py/aejay-automations/main.py
+DIR="${0:a:h}"
+
+python "$DIR/main.py"
